127fbe0c555c637b789031b49bfe5fceaf0a24c7
Aayush singh <saayush615@gmail.com>
2026-03-12 18:41:53 +0530

    feat: add core features routes on /journal endpoint

--- backend/routes/journal.js
+++ backend/routes/journal.js
@@ -1,6 +1,16 @@
 import express from 'express';
-import { } from '../controllers/journal.js'
+import { 
+    handleJournalEntry, 
+    handleGetJournalEntry, 
+    handleEmotionAnalysis, 
+    handleUserInsights 
+} from '../controllers/journal.js'
 
 const router = express.Router();
+
+router.post('/analyze', handleEmotionAnalysis);
+router.get('/insights/:userId', handleUserInsights);
+router.post('/', handleJournalEntry);
+router.get('/:userId', handleGetJournalEntry);
 
 export default router;
