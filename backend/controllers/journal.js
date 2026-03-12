8517f4c640cd29da3ecdabc57570ec5c9b249999
Aayush singh <saayush615@gmail.com>
2026-03-13 00:07:51 +0530

    feat: add journal controller functions boilerplate code

---
+++ backend/controllers/journal.js
@@ -0,0 +1,40 @@
+import Journal from "../models/journal.js";
+
+async function handleJournalEntry(req, res, next) {
+    try {
+        
+    } catch (error) {
+        next(error);
+    }
+}
+
+async function handleGetJournalEntry(req, res, next) {
+    try {
+        
+    } catch (error) {
+        next(error);
+    }
+}
+
+async function handleEmotionAnalysis(req, res, next) {
+    try {
+        
+    } catch (error) {
+        next(error);
+    }
+}
+
+async function handleUserInsights(req, res, next) {
+    try {
+        
+    } catch (error) {
+        next(error);
+    }
+}
+
+export {
+    handleJournalEntry, 
+    handleGetJournalEntry, 
+    handleEmotionAnalysis, 
+    handleUserInsights 
+}
