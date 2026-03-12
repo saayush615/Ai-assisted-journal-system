ba9c967933338f8c7454a35f8cf098a87bb6c5a1
Aayush singh <saayush615@gmail.com>
2026-03-12 17:18:11 +0530

    feat: add database connection setup with mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,13 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+import { connectToDB } from './config/database.js'
+
+dotenv.config();
 
 const app = express();
+
+connectToDB();
 
 app.get('/', (req,res) => {
     return res.status(200).json({
