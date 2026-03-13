739cd5ebd784b16b99eb1bdf01e9f9b2e29b142f
Aayush singh <saayush615@gmail.com>
2026-03-13 08:21:01 +0530

    feat: add handleJournalEntry controller function

--- backend/controllers/journal.js
+++ backend/controllers/journal.js
@@ -2,7 +2,40 @@ import Journal from "../models/journal.js";
 
 async function handleJournalEntry(req, res, next) {
     try {
-        
+        const userId = req.user?.id;
+        if (!userId) {
+            return res.status(401).json({
+                success: false,
+                message: 'Login before submitting journal'
+            });
+        };
+
+        const { ambience, text } = req.body;
+
+        if (!['forest', 'ocean', 'mountain'].includes(ambience)) {
+            return res.status(400).json({
+                success: false,
+                message: 'You can only choose the ambience between forest, ocean, mountain options'
+            });
+        }
+
+        if (!text?.trim()) {
+            return res.status(400).json({
+                success: false,
+                message: 'You must write some text.'
+            });
+        }
+
+        const response = await Journal.create({
+            userId,
+            ambience,
+            text: text.trim()
+        })
+        return res.status(201).json({
+            success: true,
+            message: 'New journal entry created!',
+            response
+        })
     } catch (error) {
         next(error);
     }
