7ea3cc91473b884bcf1936a13463e05fc2272a9d
Aayush singh <saayush615@gmail.com>
2026-03-13 09:14:43 +0530

    feat: Get all the journal entries by userId

--- backend/controllers/journal.js
+++ backend/controllers/journal.js
@@ -43,7 +43,37 @@ async function handleJournalEntry(req, res, next) {
 
 async function handleGetJournalEntry(req, res, next) {
     try {
-        
+        const { userId } = req.params;
+        const authUserId = req.user?.id;
+        if (!userId) {
+            return res.status(401).json({
+            success: false,
+            message: 'Login to get your journal entries.'
+            });
+        };
+
+        // Authrization
+        if (authUserId !== userId) {
+            return res.status(403).json({
+                success: false,
+                message: 'Forbidden'
+            });
+        }
+
+        const response = await Journal.find({ userId });
+        if (response.length === 0) {
+            return res.status(200).json({
+            success: true,
+            message: 'You have no journal entry yet!',
+            response: []
+        })
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: 'Successfully fetched journal entries',
+            response
+        })
     } catch (error) {
         next(error);
     }
