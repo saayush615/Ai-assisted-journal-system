9ce145714f196a53318d2df306827d556c171d35
Aayush singh <saayush615@gmail.com>
2026-03-15 21:32:50 +0530

    refactor: removed validation for testing purpose

--- backend/models/auth.js
+++ backend/models/auth.js
@@ -9,12 +9,12 @@ const authSchema = mongoose.Schema({
         type: String,
         unique: true,
         required: true,
-        match: [/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, 'Please enter a valid email']
+        // match: [/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, 'Please enter a valid email']
     },
     password: {
         type: String,
         required: true,
-        minlength: [6, 'Password should be at least 6 characters'],
+        // minlength: [6, 'Password should be at least 6 characters'],
         select: false
     }
 }, {timestamps: true});
